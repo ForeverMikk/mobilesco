d886da048fa8bc2b07fea8e8fcb2795bf0d9bf65
unknown <michaelmolina679@gmail.com>
2022-07-16 16:20:51 -0500

    add about us section

--- src/views/AboutUs/AboutUs.jsx
+++ src/views/AboutUs/AboutUs.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
 
+import './AboutUs.scss';
+import aboutBackground from '../../asstes/img/about-us-background.png';
+import politicImg from '../../asstes/img/hand-shake.png';
+
+
+const Politic = ({icon, title, description}) => {
+    return (
+        <div className='politic'>
+            <img src={icon} alt={title} />
+            <div className="info">
+                <h3>{title}</h3>
+                <p>{description}</p>
+            </div>
+        </div>
+    )
+}
 const AboutUs = () => {
-    return <h1>Hola Sobre Nosotros</h1>
+    return (
+        <section className='about-us'>
+            <h1 className="title">Los años nos respaldan... </h1>
+
+            <div className="img">
+                <img src={aboutBackground} alt="Short video of introduction" />
+            </div>
+
+            <div className="politics">
+               <Politic title="Políticas de retorno accesibles" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit" icon={politicImg} />
+               <Politic title="Políticas de retorno accesibles" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit" icon={politicImg} />
+               <Politic title="Políticas de retorno accesibles" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit" icon={politicImg} />
+            </div>
+            
+        </section>
+    )
 }
 
 export default AboutUs;
