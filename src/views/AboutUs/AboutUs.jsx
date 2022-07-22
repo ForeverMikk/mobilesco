fce99ba49c40fd935512597a12fd534e57c25cea
unknown <michaelmolina679@gmail.com>
2022-07-22 10:03:34 -0500

    finish service card

--- src/views/AboutUs/AboutUs.jsx
+++ src/views/AboutUs/AboutUs.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 import './AboutUs.scss';
+import serviceImg from '../../asstes/img/services.jpg';
+import handShake from '../../asstes/img/hand-shake.png';
+import shoopingCart from '../../asstes/img/shopping-cart.png';
+import supportAgent from '../../asstes/img/support-agent.png';
+import truck from '../../asstes/img/truck.png';
+
 import aboutBackground from '../../asstes/img/about-us-background.png';
 import politicImg from '../../asstes/img/hand-shake.png';
+import ServiceCard from './ServiceCard/ServiceCard';
 
 
 const Politic = ({icon, title, description}) => {
@@ -30,7 +37,40 @@ const AboutUs = () => {
                <Politic title="Políticas de retorno accesibles" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit" icon={politicImg} />
                <Politic title="Políticas de retorno accesibles" description="Lorem ipsum dolor sit amet, consectetuer adipiscing elit" icon={politicImg} />
             </div>
+
+            <div className="services">
+                <ServiceCard 
+                    img={serviceImg} 
+                    title="El precio que mejor se te acomoda" 
+                    icon={handShake} 
+                    description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat" 
+                />
+                <ServiceCard 
+                    img={serviceImg} 
+                    title="El precio que mejor se te acomoda" 
+                    icon={shoopingCart} 
+                    reverse="reverse"
+                    description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat" 
+                />
+                <ServiceCard 
+                    img={serviceImg} 
+                    title="El precio que mejor se te acomoda" 
+                    icon={supportAgent} 
+                    description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat" 
+                />
+                <ServiceCard 
+                    img={serviceImg} 
+                    title="El precio que mejor se te acomoda" 
+                    icon={truck} 
+                    reverse="reverse"
+                    description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat" 
+                />
+            </div>
             
+
+            <div className="galery">
+                
+            </div>
         </section>
     )
 }
--- src/views/AboutUs/ServiceCard/ServiceCard.jsx
+++ src/views/AboutUs/ServiceCard/ServiceCard.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-const ServiceCard = () => {
-    return (
-        <div>
 
+import './ServiceCard.scss';
+const ServiceCard = ({img, title, description, icon, reverse}) => {
+    return (
+        <div className={`service-card ${reverse}`}>
+            <div className="img">
+                <img src={img} alt={title} />
+            </div>
+
+            <div className="service-info">
+                <img src={icon} alt={`Service ${title}`} />
+                <h3 className='title'>{title}</h3>
+                <p className="description">{description}</p>
+            </div>
         </div>
     )
 }
