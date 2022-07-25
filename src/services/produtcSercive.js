33103137c491743776bdaf7d2aa38616000fd049
unknown <michaelmolina679@gmail.com>
2022-07-25 18:03:06 -0500

    add first product function in home

---
+++ src/services/produtcSercive.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const MOBILESCO_URL = 'https://mobilesco.mx/API/muebles'
+
+export const getAllProducts = async() => {
+    const response = await axios.get(MOBILESCO_URL);
+    const data = response.data;
+
+    return data;
+    
+}
+
+export const getProductById = async(id) => {
+    const response = await axios.get(MOBILESCO_URL + `?idMueble=${id}`);
+    const data = response.data;
+
+    // console.log('DATA',data)
+    return data;
+}
+
+export const getProductsByCategory = async(category) => {
+    const response = await axios.get(MOBILESCO_URL + `?idCategoria=${category}`);
+    const data = response.data;
+
+    return data;
+}
+
+export const getProductsByType = async(type) => {
+    const response = await axios.get(MOBILESCO_URL + `?idTipo=${type}`);
+    const data = response.data;
+
+    return data;
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './Home.scss';
-import silla from './../../asstes/img/sillas/silla-nueva.png';
+// import silla from './../../asstes/img/sillas/silla-nueva.png';
 
 import ProductItem from '../../components/ProductItem/ProductItem';
 import SearchBox from '../../components/SearchBox/SearchBox';
@@ -10,17 +10,38 @@ import OurServices from './OurServices/OurServices';
 import Categories from './Categories/Categories';
 import MonthProduct from './MonthProduct/MonthProduct';
 import Suscribe from './Suscribe/Suscribe';
+import { getProductById } from '../../services/produtcSercive';
 
 
 const Home = () => {
+    
+    const [product, setProduct] = useState();
+
+    useEffect(() => {
+        const fetchData = async() => {
+            const data = await getProductById(3);
+            setProduct(data[0])
+        }
+        fetchData()
+    }, [])
+          
     return (
         <>
             {/* <Navbar /> */}
             <header>
                 <div className="background">
                     <h1 className='title'>Make your interior <br /> look better</h1>
-                    <ProductItem name="Silla nueva" description="La mejor silla de la Historia" img={silla} className='home' id="1" price="150"/>
+                    {product && <ProductItem 
+                        name={product.NOMBRE} 
+                        description={product.DESCRIPCION}
+                        img={product.IMAGEN} 
+                        className='home'
+                        id={product.CLAVE}
+                        price={product.COSTO}
+                        />
+                    }
                     <SearchBox />
+                    {/* <button onClick={handleClick}>Dale Click</button> */}
                     <p className='description'>Si estás en busca de mobiliario de calidad entonces eres bienvenido. Tenemos diferentes tipos de muebles esperando a ser repartidos.</p>
                 </div>
             </header>
