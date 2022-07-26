cb2e5075a2a1eec08c2074cb9c9fa8a01aa22a1b
unknown <michaelmolina679@gmail.com>
2022-07-26 13:38:13 -0500

    creating search bar component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
-
 import './SearchBar.scss';
 
-const SearchBar = () => {
+const SearchBar = ({onChange, productsFiltered}) => {
+
     return (
-        <h1>Hola Barra</h1>
+        <div className='search-bar'>
+            <div className='input-box'>
+                <input type="text" placeholder='Encuentra un mueble a tu medida' onChange={onChange}/>
+                <button className='search-button'>Buscar</button>
+                <button className='categories'>Categorias</button>
+            </div>
+
+            {productsFiltered && <div className="dropdown">
+                {productsFiltered.map((item, index) => (
+                    <div key={index} className="dropdown-row">
+                        <p className='item-name'>{item.NOMBRE}</p>
+                    </div>
+                ))}
+            </div>}
+        </div>
     )
 }
 
--- src/views/CatalogSearch/CatalogSearch.jsx
+++ src/views/CatalogSearch/CatalogSearch.jsx
@@ -1,41 +1,27 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 
 import './CatalogSearch.scss';
-// import { products } from '../../variables/items';
 import ProductsList from './ProductsList/ProductList';
+import SearchBar from '../../components/SearchBar/SearchBar';
+import { getAllProducts } from '../../services/produtcSercive';
 
 const CatalogSearch = () => {
 
-    // const [productArray,  setProductArray] = useState();
-    // const [product, setProduct] = useState();
-    const [productsFiltered, setProductsFiltered] = useState(null);    
-    
-    const getProducts = async(event) => {
-       
-        const response = await axios.get('https://mobilesco.mx/API/muebles');
-
-        const data = response.data;
-
-        // console.log("DATA", data);
-        return data;
-    }
+    const [productsFiltered, setProductsFiltered] = useState();
 
     const onChange = async(event) => {
         const productInput = event.target.value.toLowerCase();
-        const productsAPI = await getProducts();
-      
+        const productsList = await getAllProducts();
 
         if(productInput.length > 2) {
-            const filtered = productsAPI.filter(item => {
+            const filtered = productsList.filter(item => {
                 const fullName = item.NOMBRE.toLowerCase();
 
-                // return fullName && fullName.match(productInput) && fullName !== productInput
-                // setProduct(fullName);
                 return fullName.indexOf(productInput) >= 0;
             })
+            console.log("filtrados",filtered)
             setProductsFiltered(filtered);
-        }
+        } 
 
         if(productInput.trim() === ''){
             setProductsFiltered(null);
@@ -44,29 +30,8 @@ const CatalogSearch = () => {
     
     return(
         <section className='catalog-search'>
-
-
-            <form className='search-bar'>
-                <div className='input-box'>
-                    <input type="text" placeholder='Encuentra un mueble a tu medida' onChange={onChange}/>
-                    <button className='search-button'>Buscar</button>
-                    <button className='categories'>Categorias</button>
-                </div>
-
-                {productsFiltered && <div className="dropdown">
-                    {productsFiltered.map((item, index) => (
-                        <div key={index} className="dropdown-row">
-                            <p className='item-name'>{item.NOMBRE}</p>
-                        </div>
-                    ))}
-                </div>}
-
-            </form>
-
-            {/* <button onClick={getProducts}>Buscar Prueba</button> */}
-
-           {productsFiltered && <ProductsList products={productsFiltered}/> }
-
+            <SearchBar onChange={onChange} productsFiltered={productsFiltered}/>
+            {productsFiltered && <ProductsList products={productsFiltered}/> }
         </section>
     )
 }
