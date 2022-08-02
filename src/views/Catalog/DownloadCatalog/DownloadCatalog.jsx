3d252cbb634f526c677a9cad335b0f1a0950b599
unknown <michaelmolina679@gmail.com>
2022-08-02 15:08:37 -0500

    new styles changes

--- src/views/Catalog/DownloadCatalog/DownloadCatalog.jsx
+++ src/views/Catalog/DownloadCatalog/DownloadCatalog.jsx
@@ -2,29 +2,50 @@ import React, { useState, useEffect } from 'react';
 
 import './DownloadCatalog.scss';
 import ProductItem from '../../../components/ProductItem/ProductItem';
-import { getProductById } from '../../../services/productSercive';
+import { getProductById, getProductImages } from '../../../services/productSercive';
 
 const DownloadCatalog = () => {
 
     const [product, setProduct] = useState();
+    const [productImages, setProductImages] = useState();
 
     useEffect(() => {
         const fetchData = async() => {
             const data = await getProductById(1);
-            // console.log('data', data[0])
-            setProduct(data[0])
+            const images = await getProductImages(1);
+            
+            setProductImages(images[2]);
+            setProduct(data)
         }
         fetchData()
     }, [])
 
     return(
         <section className='download-catalog'>
-            {product && <ProductItem name={product.NOMBRE} img={product.IMAGEN} />}
+            {product && <ProductItem 
+                        name={product.NOMBRE} 
+                        description={product.DESCRIPCION}
+                        img={`https://mobilesco.mx/API/images/uploads/${product.CLAVE}/${productImages.NOMBRE}`} 
+                        id={product.CLAVE}
+                        price={parseInt(product.COSTO)}
+                        category={product.CATEGORIA}
+                        color={product.COLOR}
+                        material={product.MATERIAL}
+                    />}
             <div className='data'>
                 <h2 className='title'>Descarga el catálogo</h2>
                 <button>PDF</button>
             </div>
-            {product && <ProductItem name={product.NOMBRE} img={product.IMAGEN} />}
+            {product && <ProductItem 
+                        name={product.NOMBRE} 
+                        description={product.DESCRIPCION}
+                        img={`https://mobilesco.mx/API/images/uploads/${product.CLAVE}/${productImages.NOMBRE}`} 
+                        id={product.CLAVE}
+                        price={parseInt(product.COSTO)}
+                        category={product.CATEGORIA}
+                        color={product.COLOR}
+                        material={product.MATERIAL}
+                    />}
         </section>
     )
 }
--- src/views/WishList/Wishlist.jsx
+++ src/views/WishList/Wishlist.jsx
@@ -80,8 +80,6 @@ const Wishlist = () => {
                 </aside>
 
             </div>
-
-
              
         </section>
     )
