b7883ff869d6d7f59a540ae6337578a2848205ea
unknown <michaelmolina679@gmail.com>
2022-08-09 18:02:36 -0500

    adding frequent questions in about us page

--- src/views/AboutUs/FrequentQuestions/FrequentQuestions.jsx
+++ src/views/AboutUs/FrequentQuestions/FrequentQuestions.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
 import './FrequentQuestions.scss';
 
 const FrequentQuestions = () => {
 
-    const [showAnswer, setShowAnswer] = useState(false);
+    const [showAnswer, setShowAnswer] = useState(
+        {show: [false, false, false, false]}
+    );
 
-    const handleClick = () => {
-        setShowAnswer(!showAnswer);
+    const changeName = () => {
+        let answer = "answer";
+        answer += this.state.show === true ? "hide" : "show";
+        return answer;
+    }
+
+    const showHide = (num) => {
+        setShowAnswer((prevState) => {
+            const newItems = [...prevState.show];
+            newItems[num] = !newItems[num];
+            return {show: newItems}
+        })
     }
 
     return (
@@ -17,42 +31,51 @@ const FrequentQuestions = () => {
             <div className="container">
                 <div className="row">
                     <div className="container-faq">
-                        <div className="item-faq">
+
+                        <div className={`item-faq ${showAnswer.show[0] ? 'active' : ''}`}>
                             <div className="question">
                                 <h3>¿A qué número me puedo contactar? </h3>
-                                <div className="more" onClick={handleClick}><i> </i></div>
+                                <div className="more" onClick={() => showHide(0)}>
+                                    <i> <FontAwesomeIcon icon={showAnswer.show[0] ? faAngleDown : faAngleLeft} size="2x" /> </i>
+                                </div>
                             </div>
-                            <div className={`answer first ${showAnswer ? 'active' : ''}`}>
+                            <div className={`answer ${showAnswer.show[0] ? 'active' : ''}`}>
                                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores repellat vitae tenetur eos doloremque. Tempora sed ut consequatur laboriosam sit mollitia beatae error, omnis deleniti, earum eius odit. Doloribus, nesciunt.<span>A</span></p>
                             </div>
                         </div>
 
-                        <div className="item-faq">
+                        <div className={`item-faq ${showAnswer.show[1] ? 'active' : ''}`}>
                             <div className="question">
                                 <h3>¿Desde qué cantidad puedo recibir un crédito? </h3>
-                                <div className="more" onClick={handleClick}><i> </i></div>
+                                <div className="more" onClick={() => showHide(1)}>
+                                    <i> <FontAwesomeIcon icon={showAnswer.show[1] ? faAngleDown : faAngleLeft} size="2x" /> </i>
+                                </div>
                             </div>
-                            <div className={`answer second ${showAnswer ? 'active' : ''}`}>
+                            <div className={`answer ${showAnswer.show[1] ? 'active' : ''}`}>
                                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores repellat vitae tenetur eos doloremque. Tempora sed ut consequatur laboriosam sit mollitia beatae error, omnis deleniti, earum eius odit. Doloribus, nesciunt.<span>A</span></p>
                             </div>
                         </div>
 
-                        <div className="item-faq">
+                        <div className={`item-faq ${showAnswer.show[2] ? 'active' : ''}`}>
                             <div className="question">
                                 <h3>¿Puedo caragr en cualquier gasolinería?</h3>
-                                <div className="more"  onClick={handleClick}><i> </i></div>
+                                <div className="more"  onClick={() => showHide(2)}>
+                                    <i> <FontAwesomeIcon icon={showAnswer.show[2] ? faAngleDown : faAngleLeft} size="2x" /> </i>
+                                </div>
                             </div>
-                            <div className={`answer third ${showAnswer ? 'active' : ''}`}>
+                            <div className={`answer ${showAnswer.show[2] ? 'active' : ''}`}>
                                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores repellat vitae tenetur eos doloremque. Tempora sed ut consequatur laboriosam sit mollitia beatae error, omnis deleniti, earum eius odit. Doloribus, nesciunt.<span>A</span></p>
                             </div>
                         </div>
 
-                        <div className="item-faq">
+                        <div className={`item-faq ${showAnswer.show[3] ? 'active' : ''}`}>
                             <div className="question">
                                 <h3>¿Cúanto tiempo tengo para pagar mi crédito? </h3>
-                                <div className="more" onClick={handleClick}><i> </i></div>
+                                <div className="more" onClick={() => showHide(3)}>
+                                    <i> <FontAwesomeIcon icon={showAnswer.show[3] ? faAngleDown : faAngleLeft} size="2x" /> </i>
+                                </div>
                             </div>
-                            <div className={`answer fourth ${showAnswer ? 'active' : ''}`}>
+                            <div className={`answer ${showAnswer.show[3] ? 'active' : ''}`}>
                                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores repellat vitae tenetur eos doloremque. Tempora sed ut consequatur laboriosam sit mollitia beatae error, omnis deleniti, earum eius odit. Doloribus, nesciunt.<span>A</span></p>
                             </div>
                         </div>
