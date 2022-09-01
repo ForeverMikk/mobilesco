97c0c206586760efd256ac3d16eec880e51abb90
unknown <michaelmolina679@gmail.com>
2022-08-31 19:16:29 -0500

    add translations in wishlist

--- src/views/WishList/Wishlist.jsx
+++ src/views/WishList/Wishlist.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 
 import './Wishlist.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,14 +8,11 @@ import { faUser, faHeart, faCheck } from '@fortawesome/free-solid-svg-icons';
 import PreviewProduct from './PreviewProduct/PreviewProduct';
 import { sendEmailData } from '../../services/emailService';
 
-const scrollTop = () => {
-    const scrolledElement = document.getElementById('catalog');
 
-    scrolledElement.scrollIntoView();
-}
 
 const Wishlist = () => {
 
+    const { t }= useTranslation();
     const cartProducts = useSelector(state => state.cart.itemList);
     const [addressInfo, setAddressInfo] = useState(
         {
@@ -82,18 +80,24 @@ const Wishlist = () => {
     }
 
     useEffect(() => {
-      scrollTop();
+        const scrollTop = () => {
+            const scrolledElement = document.getElementById('catalog');
+        
+            scrolledElement.scrollIntoView();
+        }
+
+        scrollTop();
     }, [])
     
 
     return(
-        <section className="wishlist">
-            <h1 className="title">Solicitud de cotización</h1>
+        <section className="wishlist" id='catalog'>
+            <h1 className="title">{t('wishlist.title')}</h1>
 
             <div className="status-bar">
-                <p><FontAwesomeIcon icon={faHeart} /> Lista de deseos   ·  ·  ·  </p>
-                <p><FontAwesomeIcon icon={faUser} /> Datos de contacto   ·  ·  ·  </p>
-                <p><FontAwesomeIcon icon={faCheck} /> Confirmación   ·  ·  · </p>
+                <p><FontAwesomeIcon icon={faHeart} /> {t('wishlist.wishlist-title')}  </p>
+                <p><FontAwesomeIcon icon={faUser} /> {t('wishlist.data-title')}  </p>
+                <p><FontAwesomeIcon icon={faCheck} /> {t('wishlist.confirmation-title')} </p>
             </div>
 
             <div className="preview">
@@ -115,55 +119,55 @@ const Wishlist = () => {
                 <aside className='summary'>
                     <form id='form' onSubmit={(e) => {sendSaleData(e, cartProducts, addressInfo, userInfo, extraInfo )}}>
                         <div className="summary-resume">
-                            <h3>Resumen del pedido</h3>
+                            <h3>{t('wishlist.resume.title')}</h3>
                             {cartProducts && cartProducts.map((item, index) => (
                                 <p key={index}>
-                                Cantidad: <span>{item.cantidad}</span>
+                                {t('wishlist.resume.quantity')} <span>{item.cantidad}</span>
                                 <br />
-                                Nombre: <span>{item.name}</span>
+                                {t('wishlist.resume.name')} <span>{item.name}</span>
                                 {/* <br /> */}
                                 {/* Precio Total: <span>{item.totalPrice}</span> */}
                                 <br />
-                                Color: <span>{item.color}</span>
+                                {t('wishlist.resume.color')} <span>{item.color}</span>
                                 <br />
-                                Material: <span>{item.material}</span>
+                                {t('wishlist.resume.material')} <span>{item.material}</span>
                                 </p>
                             ))}
                         </div>
 
-                        <div className="contact-info" onChange={onChangePickUp} required>
-                            <h3>Tipo de Entrega</h3>
+                        <div className="shipping-info" onChange={onChangePickUp} required>
+                            <h3>{t('wishlist.shipping.title')}</h3>
                             <input type="radio" name="shipping" id="home-delivery" value="a domicilio"/>
-                            <label htmlFor="shipping">Envio a domicilio</label>
+                            <label htmlFor="shipping">{t('wishlist.shipping.label1')}</label>
                             <input type="radio" name="shipping" id="pick-up" value="recoger en sucursal" />
-                            <label htmlFor="shipping">Recoger en sucursal</label>
+                            <label htmlFor="shipping">{t('wishlist.shipping.label2')}</label>
                         </div>
 
                         <div className="special-info" onChange={onChangeExtra}>
-                            <h3>Especificaciones adicionales</h3>
-                            <p>Envía un comentario con las características deseadas para tu mobiliario; agrega medidas específicas, disponibilidad de colores, etc. </p>
+                            <h3>{t('wishlist.special-info.title')}</h3>
+                            <p>{t('wishlist.special-info.description')}</p>
                             <textarea name="texarea" id="texarea" cols="30" rows="10" required></textarea>
                         </div>
 
-                        <div className="shipping-info" onChange={onChangeAddress}>
-                            <h3>Zona de Entrega</h3>
-                            <input type="text" name='calle' value={addressInfo.calle} id='calle' placeholder='Dirección' required/>
-                            <input type="text" name='lugar' value={addressInfo.lugar} id='lugar' placeholder='Ciudad o Localidad' required/>
+                        <div className="address-info" onChange={onChangeAddress}>
+                            <h3>{t('wishlist.address-info.title')}</h3>
+                            <input type="text" name='calle' value={addressInfo.calle} id='calle' placeholder={t('wishlist.address-info.address')} required/>
+                            <input type="text" name='lugar' value={addressInfo.lugar} id='lugar' placeholder={t('wishlist.address-info.city')} required/>
 
                             <div className="other">
-                                <input type="number" name='numero' value={addressInfo.numero} id='numero' placeholder='Número Exterior' required/>
-                                <input type="number" name='cp' value={addressInfo.cp} id='cp' placeholder='Código Postal' required/>
+                                <input type="number" name='numero' value={addressInfo.numero} id='numero' placeholder={t('wishlist.address-info.number')} required/>
+                                <input type="number" name='cp' value={addressInfo.cp} id='cp' placeholder={t('wishlist.address-info.cp')} required/>
                             </div>
 
                             {/* <button>Cambiar</button> */}
                         </div>
 
                         <div className="user-info" onChange={onChangeUser}>
-                            <h3>Datos de Contacto</h3>
-                            <input type="text" name="name" value={userInfo.name} id="name" className="name" placeholder='Nombre' required/>
-                            <input type="email" name="email" value={userInfo.email} id="email" className="email" placeholder='Correo electrónico' required/>
-                            <input type="tel" name="phone" value={userInfo.phone} id="phone" className="phone" placeholder='Teléfono' required/>
-                            <button type='submit'>Solicitar</button>
+                            <h3>{t('wishlist.user-info.title')}</h3>
+                            <input type="text" name="name" value={userInfo.name} id="name" className="name" placeholder={t('wishlist.user-info.name')} required/>
+                            <input type="email" name="email" value={userInfo.email} id="email" className="email" placeholder={t('wishlist.user-info.email')} required/>
+                            <input type="tel" name="phone" value={userInfo.phone} id="phone" className="phone" placeholder={t('wishlist.user-info.phone')} required/>
+                            <button type='submit'>{t('wishlist.user-info.submit')}</button>
                         </div>
                     </form>
                 </aside>
