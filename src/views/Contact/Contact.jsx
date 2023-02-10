b584b989d3e42be11352c25f18d0e8da17d22985
unknown <michaelmolina679@gmail.com>
2023-02-10 09:12:29 -0600

    updates

--- src/views/Contact/Contact.jsx
+++ src/views/Contact/Contact.jsx
@@ -3,6 +3,7 @@ import React, {useEffect} from 'react'
 import './Contact.scss';
 import GoogleCalendar from './GoogleCalendar/GoogleCalendar';
 import JoinUs from '../AboutUs/JoinUs/JoinUs';
+import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 
 
 const Contact = () => {
@@ -19,6 +20,22 @@ const Contact = () => {
     
     return (
         <section className="contact" id="contact">
+            <Tabs>
+                <TabList>
+                    <Tab>
+                        Titulo 1
+                    </Tab>
+                    <Tab>
+                        Titulo 2
+                    </Tab>
+                </TabList>
+                <TabPanel>
+                    <h2>Panel 1</h2>
+                </TabPanel>
+                <TabPanel>
+                    <h2>Panel 2</h2>
+                </TabPanel>
+            </Tabs>
             <GoogleCalendar />
             <JoinUs />
         </section>
